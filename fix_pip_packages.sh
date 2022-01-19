0654ec040df7c0adde5dc9b4dd48cf51cc8b938b
Alyetama <56323389+Alyetama@users.noreply.github.com>
2022-01-19 17:53:59 -0500

    update:
    Former-commit-id: 20c2432b9f43f55664db39611aa60798380c0de8

--- fix_pip_packages.sh
+++ fix_pip_packages.sh
@@ -2,3 +2,5 @@
 pip install -U --no-deps "feedparser>=6.0.8"
 pip install -U --no-deps "dateparser>=1.0.0"
 pip install --upgrade cx_Freeze
+pip install pygooglenews --ignore-installed
+pip install pygooglenews --ignore-installed -U --no-deps
