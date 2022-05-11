18682d14da75313b7847ef231c39c20a9f749093
Alyetama <56323389+Alyetama@users.noreply.github.com>
2022-05-11 05:10:21 -0400

    Add workflow

--- fix_pip_packages.sh
+++ fix_pip_packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pip install -U --no-deps "feedparser>=6.0.8"
 pip install -U --no-deps "dateparser>=1.0.0"
-pip install --upgrade cx_Freeze
-pip install pygooglenews --ignore-installed
+# pip install --upgrade cx_Freeze
+# pip install pygooglenews --ignore-installed
 pip install pygooglenews --ignore-installed -U --no-deps
